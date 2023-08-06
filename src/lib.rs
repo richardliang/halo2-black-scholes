f43a9063993ac7744cb69bbebad340fd5385ac80
Richard Liang <richardliang2015@u.northwestern.edu>
2023-08-05 22:03:53 -0700

    Add working cdf test

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ pub use fixed_point::*;
 use std::f64::consts::PI;
 use std::marker::PhantomData;
 
+#[macro_use]
+extern crate approx;
+
 #[derive(Clone, Debug)]
 pub struct BlackScholesChip<F: ScalarField> {
     pub fixed_point: FixedPointChip<F, 63>,
@@ -112,6 +115,7 @@ pub fn std_normal_cdf<F: ScalarField>(
         return ctx.load_constant(fixed_point.quantization(1.0));
     }
 
+    // Load magic numbers
     let b1 = QuantumCell::Constant(fixed_point.quantization(0.319381530));
     let b2 = QuantumCell::Constant(fixed_point.quantization(-0.356563782));
     let b3 = QuantumCell::Constant(fixed_point.quantization(1.781477937));
@@ -119,33 +123,38 @@ pub fn std_normal_cdf<F: ScalarField>(
     let b5 = QuantumCell::Constant(fixed_point.quantization(1.330274429));
     let p = QuantumCell::Constant(fixed_point.quantization(0.2316419));
     let c2 = QuantumCell::Constant(fixed_point.quantization(0.3989423));
+    // Constants
     let one = QuantumCell::Constant(fixed_point.quantization(1.0));
     let two = QuantumCell::Constant(fixed_point.quantization(2.0));
     
+    // abs(x)
     let abs_x = fixed_point.qabs(ctx, *x);
+    // 1 / (1 + p * abs(x))
     let t = {
-        let tmp1 = fixed_point.qmul(ctx, p, *x);
-        let tmp2 = fixed_point.qadd(ctx, one, tmp1);
-        fixed_point.qdiv(ctx, one, tmp2)
+        let denominator = fixed_point.qmul(ctx, p, abs_x);
+        let denominator = fixed_point.qadd(ctx, one, denominator);
+        fixed_point.qdiv(ctx, one, denominator)
     };
+    // c2 / exp((abs_x*abs_x) / 2);
     let b = {
-        let tmp1 = fixed_point.qmul(ctx, abs_x, abs_x);
-        let tmp2 = fixed_point.qdiv(ctx, tmp1, two);
-        let tmp3 = fixed_point.qexp(ctx, tmp2);
-        fixed_point.qmul(ctx, c2, tmp3)
+        let denominator = fixed_point.qmul(ctx, abs_x, abs_x);
+        let denominator = fixed_point.qdiv(ctx, denominator, two);
+        let denominator = fixed_point.qexp(ctx, denominator);
+        fixed_point.qdiv(ctx, c2, denominator)
     };
+    // 1 - b * ((((b5 * t + b4) * t + b3) * t + b2) * t + b1) * t
     let n = {
-        let tmp1 = fixed_point.qmul(ctx, b5, t);
-        let tmp2 = fixed_point.qadd(ctx, b4, tmp1);
-        let tmp3 = fixed_point.qmul(ctx, t, tmp2);
-        let tmp4 = fixed_point.qadd(ctx, b3, tmp3);
-        let tmp5 = fixed_point.qmul(ctx, t, tmp4);
-        let tmp6 = fixed_point.qadd(ctx, b2, tmp5);
-        let tmp7 = fixed_point.qmul(ctx, t, tmp6);
-        let tmp8 = fixed_point.qadd(ctx, b1, tmp7);
-        let tmp9 = fixed_point.qmul(ctx, t, tmp8);
-        let tmp10 = fixed_point.qmul(ctx, tmp9, b);
-        fixed_point.qsub(ctx, one, tmp10)
+        let res = fixed_point.qmul(ctx, b5, t);
+        let res = fixed_point.qadd(ctx, b4, res);
+        let res = fixed_point.qmul(ctx, t, res);
+        let res = fixed_point.qadd(ctx, b3, res);
+        let res = fixed_point.qmul(ctx, t, res);
+        let res = fixed_point.qadd(ctx, b2, res);
+        let res = fixed_point.qmul(ctx, t, res);
+        let res = fixed_point.qadd(ctx, b1, res);
+        let res = fixed_point.qmul(ctx, t, res);
+        let res = fixed_point.qmul(ctx, res, b);
+        fixed_point.qsub(ctx, one, res)
     };
     if fixed_point.dequantization(*x.value()) < -5.0 {
         fixed_point.qsub(ctx, one, n)
@@ -194,12 +203,8 @@ pub fn d1d2<F: ScalarField> (
 #[cfg(test)]
 mod test {
     use super::*;
-    use halo2_base::gates::builder::{
-        GateThreadBuilder,
-    };
-    use halo2_base::halo2_proofs::{
-        halo2curves::bn256::Fr,
-    };
+    use halo2_base::gates::builder::GateThreadBuilder;
+    use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
     use halo2_base::QuantumCell::{Existing, Witness, Constant};
     use halo2_base::{
         gates::{GateInstructions, GateChip, RangeChip, RangeInstructions},
@@ -220,20 +225,20 @@ mod test {
         let fixed_point = FixedPointChip::<Fr, 63>::default(lookup_bits);
         let ctx = builder.main(0);
 
-        let x = 0.1;
+        let x = 1.0;
         let x = ctx.load_witness(fixed_point.quantization(x));
         let result = std_normal_cdf(
             ctx,
             &fixed_point,
             &x,
         );
-        let test = fixed_point.dequantization(*result.value());
-        println!("{:?} {}", result, test);
+        // Dequantize result and compare
+        let result = fixed_point.dequantization(*result.value());
     
-        // let expected = [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0];
+        let expected = 0.8413447;
+        let err = 1e-7;
 
-        // result_rev.iter().zip(expected.iter()).map(|(x, y)| {
-        //     assert_eq!(x.value(), &Fr::from(*y));
-        // }).collect_vec();
+        // Check less than error threshold
+        assert_relative_eq!(result, expected, max_relative = err);
     }
 }
